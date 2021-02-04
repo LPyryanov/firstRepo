352fbfd0ba80e39444fba111c65c7ccdaf7b1e68
LPyryanov <lpyrianov@yahoo.com>
2021-02-04 22:18:13 +0300

    Add del function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!");
+        System.out.println(del(56, 23));
+    }
+
+    public static Integer del(Integer a, Integer b) {
+        return a - b;
     }
 }
